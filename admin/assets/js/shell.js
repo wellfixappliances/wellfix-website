249c1003fb8456bde2a03fd8ce57ac27ea55a444
wellfixappliances <wellfixappliances@gmail.com>
2026-05-14 12:54:16 +0400

    Update shell.js

--- admin/assets/js/shell.js
+++ admin/assets/js/shell.js
@@ -144,7 +144,7 @@ const AdminShell = {
             <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 01-3.46 0"/></svg>
           </button>
         </div>
-        <a href="${BASE}/frontend/index.html" target="_blank" class="topbar__view-site">
+        <a href="https://wellfixappliances.github.io/wellfix-website/" target="_blank" class="topbar__view-site">
           <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
           <span>View Site</span>
         </a>
